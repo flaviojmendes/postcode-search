69aa15e995f005fc3a08976a344c3c6991cdbcca
Flávio Mendes <flaviojmendes@gmail.com>
2017-04-09 13:45:23 -0300

    Implemented interface with Angular and Bootstrap. This closes #10.

---
+++ build/resources/main/static/js/postcode.js
@@ -0,0 +1,73 @@
+angular.module('postcode', [])
+    .controller('PostcodeCtrl', function($scope, $location, $http) {
+
+        $scope.submitFormAddressIe = function () {
+            var postcode = $scope.postcode;
+            var addressUrl = 'http://' + $location.host() + ':' + $location.port() + '/address/ie/' + postcode;
+
+            $http({
+              method: 'GET',
+              url: addressUrl
+            }).then(function successCallback(response) {
+                $scope.addresses = response.data;
+            }, function errorCallback(response) {
+              alert(response);
+            });
+        }
+
+        $scope.submitFormAddressGeo = function () {
+            var postcode = $scope.postcode;
+            var addressUrl = 'http://' + $location.host() + ':' + $location.port() + '/addressgeo/ie/' + postcode;
+
+            $http({
+              method: 'GET',
+              url: addressUrl
+            }).then(function successCallback(response) {
+                $scope.addresses = response.data;
+            }, function errorCallback(response) {
+              alert(response);
+            });
+        }
+
+        $scope.submitFormPositionIe = function () {
+         var postcode = $scope.postcode;
+         var addressUrl = 'http://' + $location.host() + ':' + $location.port() + '/position/ie/' + postcode;
+
+         $http({
+           method: 'GET',
+           url: addressUrl
+         }).then(function successCallback(response) {
+             $scope.addresses = response.data;
+         }, function errorCallback(response) {
+           alert(response);
+         });
+        }
+        $scope.submitFormRevGeoIe = function () {
+         var latitude = $scope.latitude;
+         var longitude = $scope.longitude;
+         var addressUrl = 'http://' + $location.host() + ':' + $location.port() + '/rgeo/ie/' + latitude + '/' + longitude + '?distance=50';
+
+         $http({
+           method: 'GET',
+           url: addressUrl
+         }).then(function successCallback(response) {
+             $scope.addresses = response.data;
+         }, function errorCallback(response) {
+           alert(response);
+         });
+     }
+
+         $scope.submitFormAddressUk = function () {
+             var postcode = $scope.postcode;
+             var addressUrl = 'http://' + $location.host() + ':' + $location.port() + '/address/uk/' + postcode;
+
+             $http({
+               method: 'GET',
+               url: addressUrl
+             }).then(function successCallback(response) {
+                 $scope.addresses = response.data;
+             }, function errorCallback(response) {
+               alert(response);
+             });
+         }
+    });
--- src/main/java/com/fexco/postcode/AppConfiguration.java
+++ src/main/java/com/fexco/postcode/AppConfiguration.java
@@ -2,9 +2,13 @@ package com.fexco.postcode;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  *
@@ -18,7 +22,18 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource("classpath:application.properties")
 @Configuration
 @EnableCaching
-public class AppConfiguration {
+public class AppConfiguration extends WebMvcConfigurerAdapter {
+
+
+    @Bean(name = "restTemplate")
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+    }
 
 
 }
